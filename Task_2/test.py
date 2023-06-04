356ab1efe73a38bfe405e4a2bac025d71880fe4e
PJulik <119886017+PJulik@users.noreply.github.com>
2023-06-04 21:00:14 +0300

    Update test.py

--- Task_2/test.py
+++ Task_2/test.py
@@ -3,7 +3,7 @@ import main
 
 class TestYandex(TestCase):
     def test_yandex(self):
-        TOKEN = 'y0_AgAAAABoU-VWAADLWwAAAADbLtr00tGudcjtSkO1fNVLDUZsWNoW9DA'
+        TOKEN = '...'
         result = main.Yandex(TOKEN).create_folder(folder_name='test')
         try:
             self.assertEqual(201, result)
